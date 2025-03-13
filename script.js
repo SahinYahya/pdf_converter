b67c3c722a8330396dbda4c3df8eed714db5cd1c
SahinYahya <yahyasahin1453@gmail.com>
2025-03-13 22:57:57 +0300

    First upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('convertToWord').addEventListener('click', function() {
+    alert('PDF → Word dönüştürme işlemi başlıyor!');
+});
+
+document.getElementById('convertToExcel').addEventListener('click', function() {
+    alert('PDF → Excel dönüştürme işlemi başlıyor!');
+});
